fb829c47ab7fbf022cbd4ac34765343e78e1236b
Rahul Ravindran <43089715+imrhlrvndrn@users.noreply.github.com>
2022-12-26 16:58:56 +0530

    Removed unused routes

--- routes/books.router.js
+++ routes/books.router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const Books = require('../models/books.model');
-const data = require('../data');
+// const data = require('../data');
 const { errorResponse, successResponse } = require('../utils/errorHandlers');
 const { CustomError } = require('../services');
 
@@ -80,59 +80,59 @@ router
         }
     });
 
-router.get('/create', async (req, res, next) => {
-    try {
-        data.forEach(async (book) => {
-            const returnedBook = await Books.findOne({ name: book.name });
-            if (returnedBook) return;
+// router.get('/create', async (req, res, next) => {
+//     try {
+//         data.forEach(async (book) => {
+//             const returnedBook = await Books.findOne({ name: book.name });
+//             if (returnedBook) return;
 
-            const newBook = new Books({
-                name: book.name,
-                cover_image: book.cover_image,
-                summary: {
-                    text: book.summary.text.slice(0, 300),
-                    excerpt: book.summary.text.slice(0, 100),
-                },
-                authors: [...book.authors],
-                genres: [...book.genre],
-                external_urls: {
-                    amazon: book.link.url.text,
-                },
-                payment: {
-                    tax: [{ name: 'GST', percentage: 18 }],
-                },
-                variants: [
-                    {
-                        type: 'ebook',
-                        price: book.price.value,
-                    },
-                    {
-                        type: 'hardcover',
-                        price: book.price.value,
-                    },
-                    {
-                        type: 'paperback',
-                        price: book.price.value,
-                    },
-                ],
-            });
+//             const newBook = new Books({
+//                 name: book.name,
+//                 cover_image: book.cover_image,
+//                 summary: {
+//                     text: book.summary.text.slice(0, 300),
+//                     excerpt: book.summary.text.slice(0, 100),
+//                 },
+//                 authors: [...book.authors],
+//                 genres: [...book.genre],
+//                 external_urls: {
+//                     amazon: book.link.url.text,
+//                 },
+//                 payment: {
+//                     tax: [{ name: 'GST', percentage: 18 }],
+//                 },
+//                 variants: [
+//                     {
+//                         type: 'ebook',
+//                         price: book.price.value,
+//                     },
+//                     {
+//                         type: 'hardcover',
+//                         price: book.price.value,
+//                     },
+//                     {
+//                         type: 'paperback',
+//                         price: book.price.value,
+//                     },
+//                 ],
+//             });
 
-            const savedBook = await newBook.save();
-            if (!savedBook)
-                return next(
-                    CustomError.serverError(
-                        'Could not create the book entity. Please try again later'
-                    )
-                );
-        });
-        res.status(201).json({
-            message:
-                'Successfully stored all the books in the DB. Click here to check books in DB https://cloud.mongodb.com/v2/607509072e6f9572e9fc6348#metrics/replicaSet/60750a80cfc0d057347cf05f/explorer/AlphaReads/books/find',
-        });
-    } catch (error) {
-        console.error('Error => ', error);
-        return next(error);
-    }
-});
+//             const savedBook = await newBook.save();
+//             if (!savedBook)
+//                 return next(
+//                     CustomError.serverError(
+//                         'Could not create the book entity. Please try again later'
+//                     )
+//                 );
+//         });
+//         res.status(201).json({
+//             message:
+//                 'Successfully stored all the books in the DB. Click here to check books in DB https://cloud.mongodb.com/v2/607509072e6f9572e9fc6348#metrics/replicaSet/60750a80cfc0d057347cf05f/explorer/AlphaReads/books/find',
+//         });
+//     } catch (error) {
+//         console.error('Error => ', error);
+//         return next(error);
+//     }
+// });
 
 module.exports = router;
